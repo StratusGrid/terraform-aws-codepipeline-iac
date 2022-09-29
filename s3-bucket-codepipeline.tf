92e043b28041ef6e0a93fef91cf42b468d2f8d19
DJ Kaswa <74205036+DJKaswa@users.noreply.github.com>
2022-09-29 15:49:36 -0500

    Add workflow integration (#4)
    Co-authored-by: JoseMotto-sg <jose.motto@stratusgrid.com>

--- s3-bucket-codepipeline.tf
+++ s3-bucket-codepipeline.tf
@@ -1,4 +1,5 @@
 #this bucket is used to store config files, etc. which are used for processing.
+#tflint-ignore: terraform_required_providers -- Ignore warning on version constraint, #tfsec:ignore:aws-s3-block-public-acls tfsec:ignore:aws-s3-block-public-policy tfsec:ignore:aws-s3-ignore-public-acls tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-specify-public-access-block -- Ignore error for not having a public ACL
 resource "aws_s3_bucket" "pipeline_resources_bucket" {
   bucket = var.name
 
@@ -24,10 +25,10 @@ resource "aws_s3_bucket" "pipeline_resources_bucket" {
     }
   }
 
-#    logging {
-#      target_bucket = var.s3_log_target_bucket
-#      target_prefix = "s3/${var.name}/"
-#    }
+  #    logging {
+  #      target_bucket = var.s3_log_target_bucket
+  #      target_prefix = "s3/${var.name}/"
+  #    }
 
   server_side_encryption_configuration {
     rule {
@@ -39,9 +40,3 @@ resource "aws_s3_bucket" "pipeline_resources_bucket" {
 
   tags = merge(var.input_tags, {})
 }
-
-# this bucket is used for logging
-# to be filled in later
-#resource "aws_kms_key" "videotoken_resources_key" {
-#  description = "This key is used to encrypt bucket objects"
-#}
